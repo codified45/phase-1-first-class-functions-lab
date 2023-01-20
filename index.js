803b8b25fc1c36dfa85ad0d93e3b2e2acba5d197
Jack Lach <jack45k@gmail.com>
2023-01-19 23:30:16 -0500

    first class functions lab - first two tests passing

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+
+const scuberDrivers = ["Jack", "John", "Andrew", "Tom"];
+
+const returnFirstTwoDrivers = function(scuberDrivers){
+    return scuberDrivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(scuberDrivers){
+    
+    return scuberDrivers.slice(-2);
+}
