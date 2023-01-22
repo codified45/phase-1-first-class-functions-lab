2bab43197a029fdc3def4e91af30f3c21e184a0e
Jack Lach <jack45k@gmail.com>
2023-01-21 19:27:43 -0500

    first class functions lab - all 13 tests passing

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ const returnLastTwoDrivers = function(scuberDrivers){
     
     return scuberDrivers.slice(-2);
 }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplyRate){
+    return function(fare){
+        const newFare = fare * multiplyRate;
+        return newFare; 
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(scuberDrivers, firstOrLastTwo){
+    return firstOrLastTwo(scuberDrivers);
+}
